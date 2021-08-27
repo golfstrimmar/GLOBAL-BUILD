d8a10b88924ae1cc85fb8e8540c0892df4350eb5
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2021-08-27 03:42:35 +0300

    init

--- src/js/common.js
+++ src/js/common.js
@@ -22,17 +22,63 @@ $(document).ready(function (e) {
     };
   });
 
+const MyQCard = () => {
+    var ul = document.querySelector(".QContent__cards");
+    let but = document.querySelector(".QCard");
 
-var ul = document.querySelector(".QContent__cards");
-let but = document.querySelector(".QCard");
-
-let singles = Array.prototype.slice.call(ul .childNodes);
-singles.forEach((cell)=>{
-    cell.addEventListener('click', function(){
-        ul.childNodes.forEach((child)=>{
-            child.classList.remove("QCard--active")
+    let singles = Array.prototype.slice.call(ul .childNodes);
+    singles.forEach((cell)=>{
+        cell.addEventListener('click', function(){
+            ul.childNodes.forEach((child)=>{
+                child.classList.remove("QCard--active")
+            });
+            cell.classList.add("QCard--active")
         });
-      cell.classList.add("QCard--active")
-    });
-})
+    })
+
+};MyQCard();
+
+
+const MyMore = () => {
+
+    let  More = document.querySelector('.more-js');
+    let Projects  = document.querySelector('.projects-js');
+    let count = 0;
+    // Projects.classList.add("more-hidden")
+    More.innerText ="Смотреть все проекты"
+
+   const MoreClick = () => {
+       Projects.classList.toggle("more-hidden")
+       More.innerText ="Свернуть проекты"
+       const on = More.classList.toggle('on')
+       MoreClick.innerText(on ? 'Закрыть' : More.innerText ="Смотреть все проекты")
+
+   }
+    More.onclick = MoreClick
+
+};MyMore();
+
+// const MyMore = () => {
+//
+//     let  More = document.querySelector('.more-js');
+//     let Hidden  = document.querySelector('.hidden-js');
+//
+//     Hidden.classList.add("more-hidden")
+//     More.innerText ="Смотреть все проекты"
+//
+//    const MoreClick = () => {
+//         console.log(More);
+//        Hidden.classList.toggle("more-hidden")
+//        More.innerText ="Свернуть проекты"
+//        const on = More.classList.toggle('on')
+//        MoreClick.innerText(on ? 'Закрыть' : More.innerText ="Смотреть все проекты")
+//    }
+//     More.onclick = MoreClick
+//
+//
+// };MyMore();
+//
+
+
+
 
